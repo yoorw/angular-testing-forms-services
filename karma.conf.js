6e2d1a27a1eeb872c499adc061e836bb8aa14f4c
yoorw <yoorw@hotmail.com>
2020-11-08 04:44:27 -0600

    added dependencies from tutorial for testing

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'PhantomJS'],
+    phantomJsLauncher: {
+      exitOnResourceError: true
+    },
     singleRun: false,
     restartOnFileChange: true
   });
